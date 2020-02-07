01d6412d026121ed2cfe52c18f2deb941f9099e4
Bruno Delor <bruno@brunodelor.info>
2020-02-07 14:03:53 +0100

    Add plane shape

---
+++ shape/plane.py
@@ -0,0 +1,25 @@
+from geometry.mesh import Mesh
+from geometry.vector import Vector
+
+
+class Plane(Mesh):
+    def __init__(self, origin: Vector = Vector(), grid_size=10, length: int = 10):
+        vertices = [[Vector(x=x, y=y) for x in range(length)] for y in range(length)]
+
+        trianglesList = []
+        for y in range(len(vertices)-1):
+            for x in range(len(vertices[y])):
+                if x == 0:
+                    continue  # skip first column
+                # first triangle
+                trianglesList.append(vertices[y][x])
+                trianglesList.append(vertices[y][x - 1])
+                trianglesList.append(vertices[y + 1][x - 1])
+
+                # second triangle
+                trianglesList.append(vertices[y][x])
+                trianglesList.append(vertices[y + 1][x - 1])
+                trianglesList.append(vertices[y + 1][x])
+
+        super().__init__(*trianglesList)
+        self.set_center(origin)
