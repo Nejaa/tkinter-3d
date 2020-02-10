c85a0f8a0553c98d4ff1448b4a7613fa8f067eda
Bruno Delor <bruno@brunodelor.info>
2020-02-10 14:39:31 +0100

    Beginning to implement pipeline steps

--- rendering/pipeline_step.py
+++ rendering/pipeline_step.py
@@ -7,9 +7,10 @@ from scene.scene import Scene
 
 class PipelineStep(ABC):
     output_queue: Queue = None
+    running: bool = False
 
     def __init__(self):
-        self.input_queue = Queue()
+        self.input_queue = Queue(maxsize=2)
 
     def fetch_scene(self) -> Scene:
         return self.input_queue.get(block=True, timeout=None)
@@ -19,13 +20,20 @@ class PipelineStep(ABC):
             return
         if self.output_queue.full():
             try:
-                self.output_queue.get()
+                self.output_queue.get()  # discard one from head to make room; frame loss
             finally:
                 pass
         try:
             self.output_queue.put_nowait(scene)
         finally:
             pass
+
+    def run(self):
+        self.running = True
+        while self.running:
+            scene = self.fetch_scene()
+            self.process_scene(scene=scene)
+            self.send_scene(scene=scene)
 
     @abstractmethod
     def process_scene(self, scene: Scene):
--- scene/scene.py
+++ scene/scene.py
@@ -1,6 +1,22 @@
+from typing import List
+
+from scene.entity import Entity
 from scene.scene_node import SceneNode
 
 
 class Scene:
     def __init__(self):
         self.scene_root = SceneNode()
+
+    def meshes(self):
+        pass
+
+    def entities(self):
+        return self.entities_from_node(self.scene_root)
+
+    def entities_from_node(self, node: SceneNode) -> List[Entity]:
+        entities = node.entities
+        for n in node.childs:
+            entities.extend(self.entities_from_node(n))
+
+        return entities
