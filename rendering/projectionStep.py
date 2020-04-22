0270dbdd225524c7727ad8dcab7b4db2d5591082
Bruno Delor <128627@supinfo.com>
2020-04-22 19:59:35 +0200

    add projection step

---
+++ rendering/projectionStep.py
@@ -0,0 +1,17 @@
+from geometry.camera import Camera
+from rendering.pipeline_step import PipelineStep
+from scene.scene import Scene
+
+
+class ProjectionStep(PipelineStep):
+    def __init__(self, camera: Camera):
+        super().__init__()
+        self.camera = camera
+
+    def process_scene(self, scene: Scene):
+        ent = scene.entities()
+        mhs = [entity.geometry for entity in ent]
+
+        for idx, mesh in enumerate(mhs):
+            mesh.project_to(camera=self.camera)
+        pass
